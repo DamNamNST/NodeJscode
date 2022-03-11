b3e6c3a4e825d47b00242b86cdac316dc046fc70
damnam <namdtph16196@fpt.edu.vn>
2022-03-11 08:27:30 +0700

    babel

---
+++ README.md
@@ -0,0 +1,4 @@
+#setup Babel
+1. npm i --save-dev babel-cli babel-preset-env babel-preset-stage-0
+2. tạo file .babelrc 
+3. sửa file package.json
--- src/routes/product.js
+++ src/routes/product.js
@@ -2,13 +2,47 @@ const { Router } = require('express');
 const { checkAuth } = require('../middlewares/checkAuth')
 
 const router = Router();
-router.get("/products", checkAuth, (req, res) => {
-  const products = [{ id: 1, name: "Post A" }, { id: 2, name: "Post c" }];
+
+//fake data
+const products = [{id: 1,name: "Product A",},{id: 2,name: "Product B",},];
+
+
+router.get("/products", checkAuth, (req, res) => {//get all items
   res.json(products);
 })
-router.post("/products", checkAuth, (req, res) => {
-  const products = [{ id: 1, name: "Post A" }, { id: 2, name: "Post c" }];
+router.get("/product/:id", (req, res) => { // get a product
+      const result = products.find(item => item.id === +req.params.id);
+      res.json(result);
+  });
+router.post("/products", checkAuth, (req, res) => {// create product
   products.push(req.body);
   res.json(products);
 })
+router.delete("/product/:id", (req, res) => { // delete product
+    const newProducts = products.filter(item => item.id !== +req.params.id);
+    res.json(newProducts);
+});
+router.put("/product/:id", (req, res) => { // update product
+    const newProducts = products.map(item => item.id === +req.params.id ? req.body : item)
+    res.json(newProducts);
+})
 module.exports = router;
+// router.get("/products", checkAuth, (req, res) => { // get all
+//   res.json(products);
+// });
+// router.get("/product/:id", (req, res) => { // get a product
+//     const result = products.find(item => item.id === +req.params.id);
+//     res.json(result);
+// });
+// router.post('/products', checkAuth, (req, res) => { // create product
+//     products.push(req.body);
+//     res.json(products);
+// });
+// router.delete("/product/:id", (req, res) => { // delete product
+//     const newProducts = products.filter(item => item.id !== +req.params.id);
+//     res.json(newProducts);
+// });
+// router.put("/product/:id", (req, res) => { // update product
+//     const newProducts = products.map(item => item.id === +req.params.id ? req.body : item)
+//     res.json(newProducts);
+// })
